c2e2189e3cd086d66c0338c8c7b400ffb8171f83
Matheus Cabral <matheuscabraldesousa@gmail.com>
2023-06-20 15:58:54 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Code to do operations with folders
+
+- List folders from a given folder
+
+- List files from a given folder
+
+- Create a folder
---
+++ Udemy.CSharp.Aula199/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+
+namespace Udemy.CSharp.Aula199
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\mathe\OneDrive\Documentos\Projetos C#\Udemy.CSharp.Aula199\myfolder";
+
+            try
+            {
+                IEnumerable<string> folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FOLDERS:");
+                foreach (string s in folders)
+                {
+                    Console.WriteLine(s);
+                }
+
+                IEnumerable<string> files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FILES:");
+                foreach (string s in files)
+                {
+                    Console.WriteLine(s);
+                }
+
+                Directory.CreateDirectory(path + @"\newfolder");
+
+            } catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
